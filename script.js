87d6db9b60a89f4656cfda992429fb12b49d19af
Rophiel <gabrielsimoesp@gmail.com>
2024-06-03 22:06:09 -0300

    atualizacao2

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function mudarImagem(id) {
+    document.getElementById(id).src = "images/coracao-vermelho-card.png"; // Muda a imagem para a segunda imagem quando o mouse passa por cima
+}
 
+function restaurarImagem(id) {
+    document.getElementById(id).src = "images/coracao-cinza-removebg-preview.png"; // Restaura a imagem original quando o mouse sai de cima
+}

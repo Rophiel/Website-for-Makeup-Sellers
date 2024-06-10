303bee7d5a531d9c9605d1ca02a6f5e027e8a3ba
Rophiel <gabrielsimoesp@gmail.com>
2024-06-10 12:36:51 -0300

    mais estilizado e arquivos de teste

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+//MUDANDO A IMAGEM DO ÍCONE DE CORAÇÃO
 function mudarImagem(id) {
     document.getElementById(id).src = "images/coracao-vermelho-card.png"; // Muda a imagem para a segunda imagem quando o mouse passa por cima
 }
 
 function restaurarImagem(id) {
     document.getElementById(id).src = "images/coracao-cinza-removebg-preview.png"; // Restaura a imagem original quando o mouse sai de cima
+}
+
+function mudarIcone(id){
+    document.getElementById(id).src = "images/bolsa-icone-rosa.png";
+}
+
+function restaurarIcone(id){
+    document.getElementById(id).src = "images/bolsa-cinza-removebg-preview.png";
 }
